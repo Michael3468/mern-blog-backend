9b4b0eb4e1093ccc6b1312439a48a9c0b1e0a1c5
mikhail <mikhail3468@yandex.ru>
2023-01-13 13:47:32 +0300

    feat: add auth/me route and checkAuth.js module

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { validationResult } from 'express-validator';
 import { registerValidation } from './validations/auth.js';
 
 import UserModel from './models/User.js';
+import checkAuth from './utils/checkAuth.js';
 
 // init server +
 mongoose
@@ -125,6 +126,28 @@ app.post('/auth/login', async (req, res) => {
 
     res.status(500).json({
       message: 'Не удалось авторизоваться',
+    });
+  }
+});
+
+app.get('/auth/me', checkAuth, async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'Пользователь не найден',
+      });
+    }
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json(userData);
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      message: 'Нет доступа',
     });
   }
 });
