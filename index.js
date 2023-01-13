a9cbaa77073c9b2966a21cc90f1eb2edccd4721a
mikhail <mikhail3468@yandex.ru>
2023-01-13 09:46:09 +0300

    feat(index.js): add token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import express from 'express';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
 import { validationResult } from 'express-validator';
 
 import { registerValidation } from './validations/auth.js';
@@ -10,7 +11,7 @@ import UserModel from './models/User.js';
 mongoose
   .set('strictQuery', false)
   .connect(
-    'mongodb+srv://user432654:D2da5Hsg9Iu43F8mf@cluster0.b13o4kt.mongodb.net/blog?retryWrites=true&w=majority'
+    'mongodb+srv://user432654:D2da5Hsg9Iu43F8mf@cluster0.b13o4kt.mongodb.net/blog?retryWrites=true&w=majority',
   )
   .then(() => console.log('DataBase: Ok'))
   .catch((err) => console.log('DataBase error: ', err));
@@ -39,27 +40,42 @@ app.post('/auth/register', registerValidation, async (req, res) => {
     if (!errors.isEmpty()) {
       return res.status(400).json(errors.array());
     }
-  
+
     const password = req.body.password;
     const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
-  
+    const hash = await bcrypt.hash(password, salt);
+
     const doc = new UserModel({
       email: req.body.email,
       fullName: req.body.fullName,
       avatarUrl: req.body.avatarUrl,
-      passwordHash,
-    })
-  
+      passwordHash: hash,
+    });
+
     const user = await doc.save();
 
-    res.json(user);
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      },
+    );
+
+    const { passwordHash, ...userData } = user._doc; // del passwordHash from request
+
+    res.json({
+      // ...user._doc, // doc with passwordHash
+      ...userData,
+      token,
+    });
   } catch (err) {
     console.log(err);
 
     res.status(500).json({
       message: 'Не удалось зарегистрироваться',
-    })
+    });
   }
 });
-
