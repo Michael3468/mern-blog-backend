20e8d53ca932a75bd482741763fd6415d88c28da
mikhail <mikhail3468@yandex.ru>
2023-01-13 09:03:49 +0300

    feat(index.js): add user registration in mongodb

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+import bcrypt from 'bcrypt';
 import express from 'express';
 import mongoose from 'mongoose';
 import { validationResult } from 'express-validator';
 
 import { registerValidation } from './validations/auth.js';
 
+import UserModel from './models/User.js';
+
 mongoose
   .set('strictQuery', false)
   .connect(
-    'mongodb+srv://user432654:D2da5Hsg9Iu43F8mf@cluster0.b13o4kt.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://user432654:D2da5Hsg9Iu43F8mf@cluster0.b13o4kt.mongodb.net/blog?retryWrites=true&w=majority'
   )
   .then(() => console.log('DataBase: Ok'))
   .catch((err) => console.log('DataBase error: ', err));
@@ -30,14 +33,33 @@ app.get('/', (req, res) => {
 });
 // server test -
 
-app.post('/auth/register', registerValidation, (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
-  }
+app.post('/auth/register', registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+  
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+  
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    })
+  
+    const user = await doc.save();
 
-  res.json({
-    success: true,
-  });
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      message: 'Не удалось зарегистрироваться',
+    })
+  }
 });
 
