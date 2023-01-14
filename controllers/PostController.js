65c76e587d93c6bf7f22961a09852da237ab812c
mikhail <mikhail3468@yandex.ru>
2023-01-14 11:45:58 +0300

    feat(postcontroller): add 'getOne' posts route

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,46 @@
 import PostModel from '../models/Post.js';
 
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: 'after',
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+
+          return res.status(500).json({
+            message: 'Не удалось вернуть статью',
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: 'Статья не найдена',
+          });
+        }
+
+        res.json(doc);
+      },
+    );
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      message: 'Не удалось получить статью',
+    });
+  }
+};
+
 export const getAll = async (req, res) => {
   try {
     const posts = await PostModel.find().populate('user').exec();
