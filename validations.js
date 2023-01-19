83a4d450fcb9a717d9ea7017e3fe1e8a7383dde7
mikhail <mikhail3468@yandex.ru>
2023-01-19 09:29:22 +0300

    fix(validations.js): change 'postCreateValidation' tags type to array

--- validations.js
+++ validations.js
@@ -15,6 +15,6 @@ export const registerValidation = [
 export const postCreateValidation = [
   body('title', 'Введите заголовок статьи').isLength({ min: 3 }).isString(),
   body('text', 'Введите текст статьи').isLength({ min: 3 }).isString(),
-  body('tags', 'Неверный формат тегов (укажите массив)').optional().isString(),
+  body('tags', 'Неверный формат тегов (укажите массив)').optional().isArray(),
   body('imageUrl', 'Неверная ссылка на изображение').optional().isString(),
-]
+];
