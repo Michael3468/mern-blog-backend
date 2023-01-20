fefe4f0b1e8f5bf9bbaf6f253f87f7c69bcd7d29
mikhail <mikhail3468@yandex.ru>
2023-01-20 07:00:22 +0300

    feat: add 'getLastTags' PostController and '/tags' route

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
   });
 });
 
+app.get('/tags', PostController.getLastTags);
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
