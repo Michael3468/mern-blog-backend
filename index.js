0c400de8f69ad0a8a5dc6be053e03cb4ade1b0c4
mikhail <mikhail3468@yandex.ru>
2023-01-27 14:38:33 +0300

    refactor: move multer init out of server init

--- index.js
+++ index.js
@@ -23,20 +23,6 @@ mongoose
 
 const app = express();
 
-const storage = multer.diskStorage({
-  destination: (_, __, cb) => {
-    if (!fs.existsSync('uploads')) {
-      fs.mkdirSync('uploads');
-    }
-    cb(null, 'uploads');
-  },
-  filename: (_, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage });
-
 app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads')); // use static folder 'uploads'
@@ -50,11 +36,27 @@ app.listen(4444, (err) => {
 });
 // server init -
 
-// server test +
+// server test (can be deleted) +
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
-// server test -
+// server test (can be deleted) -
+
+// upload files to server +
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+// upload files to server -
 
 app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
