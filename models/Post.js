c90e952c71114f3a960834de40ceff64bbe3e08e
mikhail <mikhail3468@yandex.ru>
2023-02-08 11:09:04 +0300

    feat: add commentsCount to post

--- models/Post.js
+++ models/Post.js
@@ -19,6 +19,10 @@ const PostSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    commentsCount: {
+      type: Number,
+      default: 0,
+    },
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
